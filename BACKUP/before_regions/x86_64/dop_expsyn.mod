0dabb9fbe942a849f40f830af593f8a70d157db6
Marcin <m.kepa12@gmail.com>
2018-02-13 16:54:13 +0100

    cleanup

--- BACKUP/before_regions/x86_64/dop_expsyn.mod
+++
@@ -1,1 +0,0 @@
-../dop_expsyn.mod
--- Things/arg_tt.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-# parser.add_argument('--val', '-v', type=float, required=True)
-parser.add_argument('--val', '-v', type=float, default=1.25)
-ag = parser.parse_args()
-print ag.val
